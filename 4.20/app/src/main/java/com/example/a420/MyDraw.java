886be8c55fdcab5f42a9d124c3262f3c3f962bbd
Krasav4ik <zub4encko.kostya@yandex.ru>
2020-03-22 21:54:01 +0300

    Java

---
+++ 4.20/app/src/main/java/com/example/a420/MyDraw.java
@@ -0,0 +1,19 @@
+package com.example.a420;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+class MyDraw extends View {
+
+
+    public MyDraw(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+
+    }
+}
