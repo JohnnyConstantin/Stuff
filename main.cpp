1e5ee27718b4ad3ff1a81fa8e666673a32e5b8f7
Krasav4ik <zub4encko.kostya@yandex.ru>
2020-04-04 21:50:27 +0300

    File sort

--- main.cpp
+++ main.cpp
@@ -2,10 +2,9 @@
 #include <fstream>
 #include <cstdlib>
 #include <vector>
-#include <cmath>
 using namespace std;
 
-/*    Written by Zubchenko Konstantin (25.03.2020)
+/*    Written by Zubchenko Konstantin (2.04.2020)
  *    As homework for "Algorithmes and data structures" classes
  *    Professor: Syromyatnikov Vladislav Petrovich
  *
@@ -13,65 +12,155 @@ using namespace std;
  *    Опущена проверка пред- и постусловий в функциях в связи с узкой направленностью программы.
  *    Ошибки возможны при некорректном вводе данных.
  */
-unsigned long long n;
+using namespace std;
 
-// Функция заполнения файла единицами
-void create_file(ofstream& file, int count) {
-    for (auto i = 0; i < count; i++) {
-        file << 1;
-    }
+unsigned long long int time1, time2, position1, position2 = -1, C = 0, n, digit, pos;
+
+void create(int* ar, unsigned long long int n, unsigned long long int pos){
+    for (int i = 0;i < n;i++)
+        ar[i] = 1;
+
+    ar[pos] = 2;
+    ofstream file("/Applications/GitHub/GitHub/Stuff/d.txt");
+    for (int i = 0;i < n;i++)
+        file << ar[i];
+    file.close();
 }
-// Определение номера элемента последовательности nc , которому будет присвоено значение равное двум
-    int position = rand() % (n - 4) + 4;
 
-// Функция линейного поиска
-    bool search_number(ifstream& f, long digit, bool showtime)
+bool search(int* ar, int digit, bool flag)
+{
+    time1 = clock();
+    for (int i = 0;i < n;i++)
     {
-        double time = clock(); //Время в начале поиска
-        long c = 0, pos = -1;
-        while (!f.eof())
+        C++;
+        if (ar[i] == digit)
         {
-            string str;
-            getline(f, str);
-            for (auto ch : str)
+            cout << "\nПозиция цифры \""
+                 << digit << "\": " << i;
+            if (flag)
             {
-                if (isdigit(ch))
+                time2 = clock();
+                cout << " C = " << C;
+                cout << " Время поиска: " << time2 - time1 << " мс";
+            }
+            return true;
+        }
+    }
+
+    cout << "\nЧисло \"" << digit << "\" не найдено!";
+    if (flag)
+    {
+        time2 = clock();
+        cout << " Время поиска : " << time2 - time1 << " мс";
+    }
+    return false;
+}
+
+bool search_file(int digit, bool flag)
+{
+    time1 = clock();
+    ifstream file2("/Applications/GitHub/GitHub/Stuff/d.txt");
+    while (!file2.eof())
+    {
+        string str;
+        getline(file2, str);
+        for (char ch : str)
+        {
+            if (isdigit(ch))
+            {
+                C++;
+                position2++;
+                if (digit == atoi(&ch))
                 {
-                    c++;
-                    pos++;
-                    if (digit == atoi(&ch))
+                    time2 = clock();
+                    cout << "\nПозиция цифры \""
+                         << digit << "\": " << position2;
+                    if (flag)
                     {
-                        cout << "\nПоиск в файле. Позиция цифры \""
-                        << digit << "\": " << pos << " Количество сравнений: " << c;
-                        if (showtime)
-                            cout << " Время поиска: " << setw(5) << (clock() - time) / CLOCKS_PER_SEC «" секунд.";
-                        return true;
+                        cout << " C = " << C;
+                        cout << " Время поиска: "<<time2-time1<<" мс";
                     }
+                    return true;
                 }
             }
         }
-
-        cout << "\nПоиск в файле. Число \"" << digit << "\" не найдено! Время поиска: " << (clock() - time) / CLOCKS_PER_SEC;
-        return false;
     }
-
-
-// Обращение к функции линейного поиска для среднего и наихудшего случаев
-    void search_numbers(ifstream& f, const string& path, bool showtime)
+    file2.close();
+    cout << "\nЧисло \"" << digit << "\" не найдено!";
+    if (flag)
     {
-        search_number(f, 2, showtime);
-        f.close();
-
-        f.open(path);
-        search_number(f, 3, showtime);
-        f.close();
+        time2 = clock();
+        cout << " Время поиска : " << time2 - time1 << " мс";
     }
+    return false;
+}
 
+void test()
+
+{
+    cout << "Тестовый прогон массива из 10 элементов:" << endl;
+    n = 10;
+    int* ar = new int[n];
+    cout << "Введите 10 чисел" << endl;
+    for (int i = 0;i < n;i++)
+        cin >> ar[i];
+
+    ofstream file("/Applications/GitHub/GitHub/Stuff/d.txt");
+    for (int i = 0;i < n;i++)
+        file << ar[i];
+    file.close();
+
+    cout << "\nПоиск в массиве." <<
+    "\nСредний случай:";
+    search(ar, 2, false);
+    cout << "\nХудший случай:";
+    search(ar, 3, false);
+    C = 0;position1 = 0;position2 = -1;
+    cout << endl << endl;
+    cout << "\nПоиск в файле." <<
+    "\nСредний случай:";
+    search_file(2, false);
+    cout << "\nХудший случай:";
+    search_file(3, false);
+    C = 0;position1 = 0;position2 = -1;
+    cout << endl << endl;
+    delete[] ar;
+}
+
+void control()
+
+{
+    cout << "Контрольный прогон:";
+    cout << endl << "----------------------------------------------------------------------------" << endl;
+    n = 5000000;
+    for (int i = 0;i < 3;i++)
+    {
+        n += 1000000;
+        cout << "Количество элементов: " << n << endl;
+        pos = rand() % (n - 4) + 4;
+        int* ar = new int[n];
+        cout << "\nПоиск в массиве." << "\nСредний случай:";
+        create(ar, n, pos);
+        search(ar, 2, true);
+        cout << "\nНаихудший случай:";
+        C = 0;
+        search(ar, 3, true);
+        C = 0;position1 = 0;position2 = -1;
+        cout<< "\n\n**************************************************************************************" << endl;
+        cout << "\nПоиск в файле." << "\nСредний случай:";
+        search_file(2, true);
+        cout << "\nНаихудший случай:";
+        C = 0;
+        search_file(3, true);
+        C = 0;position1 = 0;position2 = -1;
+        cout << endl << "----------------------------------------------------------------------------" << endl;
+        delete[] ar;
+    }
+}
 
 int main()
 {
     setlocale(LC_ALL, "Rus");
-    ifstream f;
-    string path = ""
-    search_numbers(f, )
+    test();
+    control();
 }
