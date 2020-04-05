9378d4994b137dc4197954849f10fc2c49520ac8
Krasav4ik <zub4encko.kostya@yandex.ru>
2020-04-05 23:49:03 +0300

    Little fixes

--- main.cpp
+++ main.cpp
@@ -1,166 +1,196 @@
 #include <iostream>
-#include <fstream>
 #include <cstdlib>
 #include <vector>
+#include <cmath>
 using namespace std;
 
-/*    Written by Zubchenko Konstantin (2.04.2020)
- *    As homework for "Algorithmes and data structures" classes
- *    Professor: Syromyatnikov Vladislav Petrovich
- *
- *
- *    Опущена проверка пред- и постусловий в функциях в связи с узкой направленностью программы.
- *    Ошибки возможны при некорректном вводе данных.
- */
-using namespace std;
+/* Written by Zubchenko Konstantin (25.03.2020)
+* As homework for "Algorithmes and data structures" classes
+* Professor: Syromyatnikov Vladislav Petrovich
+*
+*
+* Опущена проверка пред- и постусловий в функциях в связи с узкой направленностью программы.
+* Ошибки возможны при некорректном вводе данных.
+*/
 
-unsigned long long int time1, time2, position1, position2 = -1, C = 0, n, digit, pos;
+unsigned long long C = 0, //Количество сравнений
+        D = 0, //Глубина рекурсии
+        N; //Количество элементов
+unsigned long t; //Переменная времени
 
-void create(int* ar, unsigned long long int n, unsigned long long int pos){
-    for (int i = 0;i < n;i++)
-        ar[i] = 1;
+void insertionSort(unsigned long long n, int mas[n]){
+//В предусловии ожидается целочисленное значение типа unsigned long long и массив целочисленных значений
+    for (int j = 1; j < n; j++){
+        int key1 = mas[j];
+        int i = j - 1;
 
-    ar[pos] = 2;
-    ofstream file("/Applications/GitHub/GitHub/Stuff/d.txt");
-    for (int i = 0;i < n;i++)
-        file << ar[i];
-    file.close();
-}
-
-bool search(int* ar, int digit, bool flag)
-{
-    time1 = clock();
-    for (int i = 0;i < n;i++)
-    {
-        C++;
-        if (ar[i] == digit)
-        {
-            cout << "\nПозиция цифры \""
-                 << digit << "\": " << i;
-            if (flag)
-            {
-                time2 = clock();
-                cout << " C = " << C;
-                cout << " Время поиска: " << time2 - time1 << " мс";
-            }
-            return true;
+        while (i >= 0 && mas[i] > key1){
+            mas[i + 1] = mas[i];
+            i--;
         }
+        mas[i + 1] = key1;
     }
-
-    cout << "\nЧисло \"" << digit << "\" не найдено!";
-    if (flag)
-    {
-        time2 = clock();
-        cout << " Время поиска : " << time2 - time1 << " мс";
-    }
-    return false;
 }
 
-bool search_file(int digit, bool flag)
-{
-    time1 = clock();
-    ifstream file2("/Applications/GitHub/GitHub/Stuff/d.txt");
-    while (!file2.eof())
-    {
-        string str;
-        getline(file2, str);
-        for (char ch : str)
-        {
-            if (isdigit(ch))
-            {
-                C++;
-                position2++;
-                if (digit == atoi(&ch))
-                {
-                    time2 = clock();
-                    cout << "\nПозиция цифры \""
-                         << digit << "\": " << position2;
-                    if (flag)
-                    {
-                        cout << " C = " << C;
-                        cout << " Время поиска: "<<time2-time1<<" мс";
-                    }
-                    return true;
-                }
-            }
+class Binary{
+public:
+    static void search(unsigned long long n, int arr[n], int key){
+//В предусловии ожидается целочисленное значение типа unsigned long long, массив целочисленных значений и
+//целочисленное значение ключа поиска
+        t = clock();
+// создали массив на n элементов
+        srand(time(NULL));
+        for (unsigned long long i = 0; i < n; i++) {
+            arr[i] = rand()%100000; // считываем элементы массива
         }
+
+//insertionSort(n, arr); // сортируем с помощью функции
+        sort(arr, arr + n);
+
+        bool flag = false;
+        unsigned long long l = 0; // левая граница
+        unsigned long long r = n-1; // правая граница
+        unsigned long long mid;
+
+        while (l <= r && !flag) {
+            D++;
+            mid = (l + r) / 2; // считываем срединный индекс отрезка [l,r]
+
+            if (arr[mid] == key) {flag = true; C++;} //проверяем ключ со серединным элементом
+            if (arr[mid] > key) {r = mid - 1;C++;} // проверяем, какую часть нужно отбросить
+            else {l = mid + 1;C++;}
+        }
+        t = clock() - t;
+        if (flag) cout << "Индекс элемента " << key << " в массиве равен: " << mid;
+        else cout << "\nИзвините, но такого элемента в массиве нет";
+        cout << "\nВремя t = " << t << "\t\tt(э) = " << C;
+        cout << "\nt(a) = " << log2(n) << "\t\tt(э)/t(a) = " << C/log2(n);
+        t=0;
+        C=0;
+        cout << "\nГлубина рекурсии составила: " << D;
     }
-    file2.close();
-    cout << "\nЧисло \"" << digit << "\" не найдено!";
-    if (flag)
-    {
-        time2 = clock();
-        cout << " Время поиска : " << time2 - time1 << " мс";
+
+    static void result_search(){
+
+        N = 300000;
+        cout << "\n====================================\n";
+        int key;
+        cout << "Количество элементов массива:" << N;
+        cout << "\n\nСредний случай:\n";
+        key = rand()%100000;
+        int arr[N];
+        Binary::search(N, arr, key); //В предусловии ожидается целочисленное значение типа unsigned long long, массив целочисленных значений и
+//целочисленное значение ключа поиска
+        D=0;
+        cout << "\n\nНаихудший случай:";
+        key = 100000;
+        Binary::search(N, arr, key);
+
+        cout << "\n====================================";
+        N += 100000;
+        cout << "\n\nКоличество элементов массива:" << N;
+        cout << "\n\nСредний случай:\n";
+        key = rand()%100000;
+        int *arr1= nullptr;  // объявление нового указателя
+        arr1 = new int [N];
+        Binary::search(N, arr1, key); //В предусловии ожидается целочисленное значение типа unsigned long long, массив целочисленных значений и
+//целочисленное значение ключа поиска
+        D=0;
+        cout << "\n\nНаихудший случай:";
+        key = 100000;
+        Binary::search(N, arr1, key);
+        delete[] arr1;
+        arr1 = nullptr;
+
+        cout << "\n====================================";
+        N += 100000;
+        cout << "\n\nКоличество элементов массива:" << N;
+        cout << "\n\nСредний случай:\n";
+        key = rand()%100000;
+        int *arr2= nullptr;
+        arr2 = new int [N];
+        Binary::search(N, arr2, key); //В предусловии ожидается целочисленное значение типа unsigned long long, массив целочисленных значений и
+//целочисленное значение ключа поиска
+        D=0;
+        cout << "\n\nНаихудший случай:";
+        key = 100000;
+        Binary::search(N, arr2, key);//В предусловии ожидается целочисленное значение типа unsigned long long, массив целочисленных значений и
+//целочисленное значение ключа поиска
+        delete[] arr2;
+        arr2 = nullptr;
+
+        cout << "\n====================================";
+        N += 100000;
+        cout << "\n\nКоличество элементов массива:" << N;
+        cout << "\n\nСредний случай:\n";
+        key = rand()%100000;
+        int *arr3= nullptr;
+        arr3 = new int [N];
+        Binary::search(N, arr3, key); //В предусловии ожидается целочисленное значение типа unsigned long long, массив целочисленных значений и
+//целочисленное значение ключа поиска
+        D=0;
+        cout << "\n\nНаихудший случай:";
+        key = 100000;
+        Binary::search(N, arr3, key);//В предусловии ожидается целочисленное значение типа unsigned long long, массив целочисленных значений и
+//целочисленное значение ключа поиска
+        delete[] arr3;
+        arr3 = nullptr;
+
+        cout << "\n====================================";
+        N += 100000;
+        cout << "\n\nКоличество элементов массива:" << N;
+        cout << "\n\nСредний случай:\n";
+        key = rand()%10000;
+        int *arr4= nullptr;
+        arr4 = new int [N];
+        Binary::search(N, arr4, key); //В предусловии ожидается целочисленное значение типа unsigned long long, массив целочисленных значений и
+//целочисленное значение ключа поиска
+        D=0;
+        cout << "\n\nНаихудший случай:";
+        key = 100000;
+        Binary::search(N, arr4, key);//В предусловии ожидается целочисленное значение типа unsigned long long, массив целочисленных значений и
+//целочисленное значение ключа поиска
+        delete[] arr4;
+        arr4 = nullptr;
     }
-    return false;
-}
-
-void test()
-
-{
-    cout << "Тестовый прогон массива из 10 элементов:" << endl;
-    n = 10;
-    int* ar = new int[n];
-    cout << "Введите 10 чисел" << endl;
-    for (int i = 0;i < n;i++)
-        cin >> ar[i];
-
-    ofstream file("/Applications/GitHub/GitHub/Stuff/d.txt");
-    for (int i = 0;i < n;i++)
-        file << ar[i];
-    file.close();
-
-    cout << "\nПоиск в массиве." <<
-    "\nСредний случай:";
-    search(ar, 2, false);
-    cout << "\nХудший случай:";
-    search(ar, 3, false);
-    C = 0;position1 = 0;position2 = -1;
-    cout << endl << endl;
-    cout << "\nПоиск в файле." <<
-    "\nСредний случай:";
-    search_file(2, false);
-    cout << "\nХудший случай:";
-    search_file(3, false);
-    C = 0;position1 = 0;position2 = -1;
-    cout << endl << endl;
-    delete[] ar;
-}
-
-void control()
-
-{
-    cout << "Контрольный прогон:";
-    cout << endl << "----------------------------------------------------------------------------" << endl;
-    n = 5000000;
-    for (int i = 0;i < 3;i++)
-    {
-        n += 1000000;
-        cout << "Количество элементов: " << n << endl;
-        pos = rand() % (n - 4) + 4;
-        int* ar = new int[n];
-        cout << "\nПоиск в массиве." << "\nСредний случай:";
-        create(ar, n, pos);
-        search(ar, 2, true);
-        cout << "\nНаихудший случай:";
-        C = 0;
-        search(ar, 3, true);
-        C = 0;position1 = 0;position2 = -1;
-        cout<< "\n\n**************************************************************************************" << endl;
-        cout << "\nПоиск в файле." << "\nСредний случай:";
-        search_file(2, true);
-        cout << "\nНаихудший случай:";
-        C = 0;
-        search_file(3, true);
-        C = 0;position1 = 0;position2 = -1;
-        cout << endl << "----------------------------------------------------------------------------" << endl;
-        delete[] ar;
-    }
-}
-
+};
 int main()
 {
     setlocale(LC_ALL, "Rus");
-    test();
-    control();
+    cout << "Здравствуйте, введите, пожалуйста тестовый массив для бинарного поиска:\n" << endl;
+    int ar[10];
+    int key0;
+    int m;
+    for (int i = 0;i < 10;i++)
+    {
+        cin >> m;
+        ar[i] = m;
+    }
+    cout << "Введите ключ\n";
+    cin >> key0;
+    insertionSort(10, ar);
+    cout << "Отсортированный массив: ";
+    for (int i : ar)
+    {
+        cout << i << " ";
+    }
+
+    bool flag = false;
+    unsigned long long l = 0; // левая граница
+    unsigned long long r = 9; // правая граница
+    unsigned long long mid;
+
+    while (l <= r && !flag) {
+        mid = (l + r) / 2; // считываем срединный индекс отрезка [l,r]
+
+        if (ar[mid] == key0) flag = true; //проверяем ключ со серединным элементом
+        if (ar[mid] > key0) r = mid - 1; // проверяем, какую часть нужно отбросить
+        else l = mid + 1;
+    }
+    if (flag) cout << "\nИндекс элемента " << key0 << " в массиве равен: " << mid;
+    else cout <<
+
+    "\nИзвините, но такого элемента в массиве нет";
+
+    Binary::result_search();
 }
