3b0a7f0a071ad28cc11ae3d64dfa73db4e694c7a
Krasav4ik <zkonst12@gmail.com>
2020-04-24 22:12:47 +0300

    7lab

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <list>
 
 using namespace std;
 
-/* Written by Zubchenko Konstantin (22.04.2020)
-* As homework for "Algorithmes and data structures" lab №6
+/* Written by Zubchenko Konstantin (24.04.2020)
+* As homework for "Algorithmes and data structures" lab №7
 * Professor: Syromyatnikov Vladislav Petrovich
 *
 *
@@ -13,148 +14,133 @@ using namespace std;
 * Ошибки возможны при некорректном вводе данных.
 */
 
-long long int C = -1;
-long long int M = -1;
-# define NO_OF_CHARS 256
+struct Node {
+public :
+    string name;
+    string firstname;
+    Node *next;
+    int num;
+};
 
-char par[] = "ABC";
-long long int N = 300000;
-bool found;
-int T, T2;
-
-// A utility function to get maximum of two integers
-    int max(int a, int b)
-    {
-        return (a > b) ? a : b;
-    }
-
-// The preprocessing function for Boyer Moore's bad character heuristic
-    void badCharHeuristic(string str, int size, int badchar[NO_OF_CHARS])
-    {
-        int i;
-
-// Initialize all occurrences as -1
-        for (i = 0; i < NO_OF_CHARS; i++)
-            badchar[i] = -1;
-
-// Fill the actual value of last occurrence of a character
-        for (i = 0; i < size; i++)
-            badchar[(int) str[i]] = i;
-    }
-
-    void search(string txt, string pat)
-    {
-        C=0;
-        M=0;
-        int m = pat.length();
-        int n = txt.length();
-        found = false;
-
-        int badchar[NO_OF_CHARS];
-
-        badCharHeuristic(pat, m, badchar);
-
-        int s = 0; // s is shift of the pattern with respect to text
-        while (s <= (n - m))
-        {
-            int j = m - 1;
-
-            while (j >= 0 && pat[j] == txt[s + j]) {
-                j--;
-                C++;
-            }
-            if (j < 0)
-            {
-                printf("Искомое слово находится на %d позиции", s);
-                found = true;
-                s += (s + m < n) ? m - badchar[txt[s + m]] : 1;
-
-            }
-
-            else {
-                s += max(1, j - badchar[txt[s + j]]);
-                C++;
-                M++;
-            }
+void add_First(list<Node> a, Node n){
+    n.num = 1;
+    if(!a.empty()) {
+        for (Node node : a) {
+            node.num++;
         }
+        n.next = &a.front();
+        a.push_front(n);
+    } else {
+        a.push_front(n);
+        n.next = nullptr;
     }
-void create_file(string name, char *what,long long int n){
-    ofstream file(name);
-    for (auto i = 0; i < n; i += 10){
-        if (i == n / 2)
-        {
-            file << what;
-            continue;
-        }
-        file << "JSDFGHJKLZ";
-    }
-    file.close();
-}
-    string create_arr(string name){
-    ifstream file(name);
-    string mas;
-    getline(file, mas);
-    file.close();
-    return mas;
 }
 
-    string test_arr(){
-        string test = "asdfghjklo";
-        cout << "Введите тестовый массив для поиска строки " << par << endl;
-        for(int i = 0; i < 10; i++){
-            cin >> test[i];
+int getSize(list<Node> a){
+    return a.back().num;
+}
+
+void add_Last(list<Node> a, Node n){
+    n.num = getSize(a) + 1;
+    a.push_back(n);
+    a.back().next = nullptr;
+}
+
+void del_First(list<Node> a){
+    if(getSize(a) == 0)
+        cout << "Нечего удалять";
+    else {
+        if (getSize(a) > 1) {
+            a.pop_front();
+            for (Node node : a)
+                node.num--;
+        } else {
+            a.pop_front();
         }
-        return test;
     }
-/* Driver program to test above function */
-    int main()
-    {
-        cout << "\nАлгоритм Бойера-Мура-Хорспула\n"
-                "\nТестовый прогон. \n";
-        string path = "d.txt";
+}
 
-        cout << "Средний случай: \n";
-        string test = test_arr();
-        T = clock();
-        search(test, par);
-        T2 = clock();
-        cout << "\nM(э): " << M <<"\t f(э): " << M + C <<  "\nC(э): " << C << "\t T = " << T2 - T;
-        T=0;
-        T2=0;
-
-
-        cout << "\n\nНаихудший случай: \n";
-        test = test_arr();
-        T=clock();
-        search(test, par);
-        T2=clock();
-        if(!found){
-            cout << "Слово отсутствует в тексте \n" << "M(э): " << M <<"\t f(э): " << M + C <<  "\nC(э): " << C << "\t T = " << T2-T;
-        }
-        T=0;
-        T2=0;
-
-
-        cout << "\n\n=============================================\nКонтрольный прогон программы:\nРазмер массива: " << N;
-
-        cout << "\n\nСредний случай:\n";
-        create_file(path, par, N);
-        T=clock();
-        string txt = create_arr(path);
-        search(txt, par);
-        T2=clock();
-        cout << "\nM(э): " << M <<"\t f(э): " << M + C <<  "\nC(э): " << C << "\t T = " << T2 - T;
-        T=0;
-        T2=0;
-
-         cout << "\n\nНаихудший случай \n";
-        string pat2 = "QJKLZ";
-        T=clock();
-        string txt2 = create_arr(path);
-        search(txt2, pat2);
-        T2=clock();
-        if(!found){
-            cout << "Слово отсутствует в тексте \n" << "M(э): " << M <<"\t f(э): " << M + C <<  "\nC(э): " << C << "\t T = " << T2-T;
-        }
-        return 0;
+void del_Last(list<Node> a){
+    if(getSize(a) == 0)
+        cout << "Нечего удалять";
+    else{
+        a.pop_back();
+        a.back().next = nullptr;
     }
+}
+
+Node find_First(list<Node> a, Node n){
+    for(Node d : a) {
+        if (d.firstname == n.firstname && d.name == n.name) {
+            return d;
+        }
+    }
+}
+
+int find_All(list<Node> a, Node n){
+    int count = 0;
+    for(Node d : a){
+        if(d.firstname == n.firstname && d.name == n.name){
+            ++count;
+        }
+    }
+    return count;
+}
+
+void change_First(list<Node> a, Node n, string name, string firstname){
+    for(Node d : a){
+        if(d.firstname == n.firstname && d.name == n.name){
+            d.name = name;
+            d.firstname = firstname;
+            break;
+        }
+    }
+}
+
+void change_All(list<Node> a, Node n, string name, string firstname){
+    for(Node d : a){
+        if(d.firstname == n.firstname && d.name == n.name){
+            d.name = name;
+            d.firstname = firstname;
+        }
+    }
+}
+
+void change_Number(list<Node> a, string name, string firstname, int b){
+    for(Node d : a){
+        if(b == d.num){
+            d.name = name;
+            d.firstname = firstname;
+        }
+    }
+}
+
+void print(list<Node> a){
+    for(Node d : a){
+        cout << d.firstname << " " << d.name << endl;
+    }
+}
+
+Node get_Num(list<Node> a, int b){
+    for(Node d : a){
+        if(b == d.num){
+            return d;
+        }
+    }
+}
+
+void swap(list<Node> a, int first, int second){
+    Node node1 = get_Num(a, first);
+    Node node2 = get_Num(a, second);
+    for(Node d : a){
+        if(d.num == node1.num - 1)
+            get_Num(a, )
+        if()
+
+    }
+}
+int main() {
+    setlocale(LC_ALL, "Ru");
+
+    list<Node> a;
+}
