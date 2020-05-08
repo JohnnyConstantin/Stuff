f0967fa31183a323e08f3ba7b86de27dac18a875
Krasav4ik <zkonst12@gmail.com>
2020-05-08 22:20:07 +0300

    retro

---
+++ RetrofitTest1/app/src/main/java/com/example/retrofittest/JSONPlaceHolderApi.java
@@ -0,0 +1,15 @@
+package com.example.retrofittest;
+
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface JSONPlaceHolderApi {
+    @GET("/posts/{id}")
+    public Call<Post> getPostWithID(@Path("id") int id);
+    @POST("/posts")
+    public Call<Post> postData(@Body Post data);
+}
---
+++ RetrofitTest1/app/src/main/java/com/example/retrofittest/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.retrofittest;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText name, mail, phone, pass;
+    Button reg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView textView = findViewById(R.id.textView);
+
+        NetworkService.getInstance()
+                .getJSONApi()
+                .getPostWithID(1)
+                .enqueue(new Callback<Post>() {
+                    @Override
+                    public void onResponse(@NonNull Call<Post> call, @NonNull Response<Post> response) {
+                        Post post = response.body();
+
+                        textView.append(post.getId() + "\n");
+                        textView.append(post.getUserId() + "\n");
+                        textView.append(post.getTitle() + "\n");
+                        textView.append(post.getBody() + "\n");
+                    }
+
+                    @Override
+                    public void onFailure(@NonNull Call<Post> call, @NonNull Throwable t) {
+
+                        textView.append("Error occurred while getting request!");
+                        t.printStackTrace();
+                    }
+                });
+    }
+
+
+
+}
