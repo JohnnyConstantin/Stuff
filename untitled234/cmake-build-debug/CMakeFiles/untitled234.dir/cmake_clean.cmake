455dc8171ae058f4ab564edee968804c396ae914
Krasav4ik <zkonst12@gmail.com>
2020-07-08 20:26:13 +0300

    edit

---
+++ untitled234/cmake-build-debug/CMakeFiles/untitled234.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled234.dir/main.cpp.o"
+  "untitled234"
+  "untitled234.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled234.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
