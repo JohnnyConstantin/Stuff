cb00afb69cc0c5b0c9ea68dccf2c2fe399a07c39
Krasav4ik <zkonst12@gmail.com>
2020-09-30 16:58:07 +0300

    html

--- confform/conf.py
+++ confform/conf.py
@@ -2,22 +2,26 @@ from sly import Lexer
 
 class CalcLexer(Lexer):
     # Set of token names.   This is always required
-    tokens = { GROUP, STUDENT, ASSIGN, CONTAINS, AGE, EQUALS, DASH, ID, NUMBER, TIMES, DIVIDE,
-               LPAREN, RPAREN, NEWLINE, TYPEOFDATASTUDENT, TYPEOFDATAGROUP, INSERT, COMMENT, NAME}
+    tokens = { GROUP, STUDENT, FOR, CONTAINS, AGE, EQUALS, DASH, ID, NUMBER, TIMES, DIVIDE,
+               LPAREN, RPAREN, TYPE_OF_DATA_STUDENT, TYPE_OF_DATA_GROUP, INSERT, COMMENT, NAME, ADD, TYPE_OF_DATA_AGE, END_OF_CONTAINS,
+               END_OF_ADDING}
 
     # String containing ignored characters between tokens
     ignore = ' \t , \n'
 
     # Regular expression rules for tokens
-    ID     = r'[a-zA-Z_][a-zA-Z_]*'
-    NAME = r'[TYPEOFDATASTUDENT][a-zA-Z][a-zA-z]*'
+    CONTAINS = r'\('
+    END_OF_CONTAINS = r'\)'
+    NUMBER = r'\d+'
+    TYPE_OF_DATA_AGE = r'age'
+    NAME = r'(Vasily Pupkin)|(Ivan Ivanov)'
     COMMENT = r'\#.*'
-    ID['gr'] = TYPEOFDATAGROUP
-    ID['st'] = TYPEOFDATASTUDENT
+    TYPE_OF_DATA_GROUP = r'gr'
+    TYPE_OF_DATA_STUDENT = r'st'
+    FOR = r'in'
+    ADD = r'append'
+    END_OF_ADDING = r'dneppa'
     GROUP = r'[a-zA-Z-_][a-zA-Z0-9-_]*'
-    INSERT = r'append'
-    NUMBER  = r'\d+'
-    APPEND = 
 
 if __name__ == '__main__':
     f = open('/Users/J.C/Library/Preferences/PyCharm2019.3/scratches/scratch.txt')
