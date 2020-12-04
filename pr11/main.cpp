8f4053d602c6971ed3e5c6151867f9c7a4c04eac
Krasav4ik <zkonst12@gmail.com>
2020-12-04 22:02:59 +0300

    Pr12 done

--- pr11/main.cpp
+++ pr11/main.cpp
@@ -18,71 +18,40 @@ struct Edge {
     Edge(char node1, char node2, int len) : ver1(node1), ver2(node2), len(len) {}
 };
 
-void sort_g(Edge* g[], int len) {
-    for (int i = 0; i < len; ++i) {
-        for (int j = 0; j < len - i - 1; ++j) {
-            if (*g[j] > * g[j + 1]) {
-                Edge* s;
-                s = g[j];
-                g[j] = g[j + 1];
-                g[j + 1] = s;
-            }
-        }
-    }
-}
+const int INF = 1000000000;
 
-void kruskal(Edge* g[], int len, int vrs) {
-    int n = 0, edg = 0;
-    vector<Edge*> f;
-    map<char, int> vertices;
-    for (int i = 0; i < len; ++i) {
-        vertices[g[i]->ver1] = vertices[g[i]->ver2] = 0;
-    }
-    for (int i = 0; i < len; ++i) {
-        if (edg == vrs - 1) {
-            break;
-        }
-        if (vertices[g[i]->ver2] != vertices[g[i]->ver1]) {
-            edg++;
-            if (vertices[g[i]->ver2] != 0 && vertices[g[i]->ver1] != 0) {
-                int k = vertices[g[i]->ver1];
-                for (auto o : vertices) {
-                    if (o.second == k)
-                        vertices[o.first] = vertices[g[i]->ver2];
-                }
+void bf(Edge* g[], int v, int e, int first) {
+    vector<int> d (e, INF);
+    d[first-1] =0;
+    for (int i = 0; i < v-1; ++i) {
+        for (int j = 0; j < e; ++j) {
+            d[g[j]->ver2-1] = min(d[g[j]->ver2-1], d[g[j]->ver1-1] + g[j]->len);
             }
-            else
-                vertices[g[i]->ver2] = vertices[g[i]->ver1] = vertices[g[i]->ver2] + vertices[g[i]->ver1];
-            f.push_back(g[i]);
         }
-        if (vertices[g[i]->ver1] == 0 && vertices[g[i]->ver2] == 0) {
-            edg++;
-            n++;
-            vertices[g[i]->ver1] = vertices[g[i]->ver2] = n;
-            f.push_back(g[i]);
+    for(int i=0; i <e; ++i){
+        if(d[g[i]->ver1-1] != INF && d[g[i]->ver1-1] + g[i]->len < d[g[i]->ver2-1]){
+            cout << "Граф содержит цикл отрицательного веса\nКратчайших путей не найдено\n";
+            return;
         }
     }
-    cout << "Минимальное остовное дерево графа\n";
-    cout << "Вывод представлен в виде ребер графа\n\n";
-    for (auto o : f) {
-        cout << "(" << o->ver1 << ")-" << o->len << "-(" << o->ver2 << ")\n";
+    for(int i=0; i<v; ++i){
+        cout << "Вершина: " << i+1 << "     " << "Путь: " << d[i] << "\n";
     }
-    cout << endl;
 }
 
 void test() {
-    Edge* graph[] = { new Edge('1','2',2),
-                      new Edge('1','3',6),
-                      new Edge('1','4',4),
-                      new Edge('2','4',7),
-                      new Edge('2','5',8),
-                      new Edge('3','4',8),
-                      new Edge('3','6',5),
-                      new Edge('4','5',2),
-                      new Edge('4','6',8),
-                      new Edge('5','6',4) };
-    sort_g(graph, 10);
-    kruskal(graph, 10, 6);
+    Edge* graph[] = {   new Edge(1, 3, 12),
+                        new Edge(1,2,23),
+                      new Edge(2,3,25),
+                      new Edge(2,5,22),
+                      new Edge(2,8,35),
+                      new Edge(3,4,18),
+                      new Edge(4,6,20),
+                      new Edge(5,6,23),
+                      new Edge(5,7,14),
+                      new Edge(6,7,24),
+                      new Edge(7,8,16) };
+    bf(graph, 8, 11, 1);
 
     for (auto & i : graph) {
         delete i;
@@ -90,8 +59,7 @@ void test() {
 }
 
 void input_graph() {
-    int n, l, v;
-    char n1, n2;
+    int n, l, v, n1, n2;
     cout << "Введите кол-во вершин графа:\n";
     cin >> v;
     cout << "Введите кол-во ребер графа:\n";
@@ -103,8 +71,7 @@ void input_graph() {
         cin >> n1 >> l >> n2;
         graph[i] = new Edge(n1, n2, l);
     }
-    sort_g(graph, n);
-    kruskal(graph, n, v);
+    bf(graph, v,n,1);
 
     for (auto & i : graph) {
         delete i;
