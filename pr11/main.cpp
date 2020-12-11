8fb34dd3ff7b9d1bb2dd3e05a76006d0acd0bb0d
Krasav4ik <zkonst12@gmail.com>
2020-12-11 15:36:43 +0300

    hw7 math

--- pr11/main.cpp
+++ pr11/main.cpp
@@ -1,127 +1,161 @@
 #include <iostream>
-#include <map>
 #include <vector>
+#include <math.h>
+#include<iomanip>
 using namespace std;
+float K, L;
 
-struct Edge {
-    char ver1, ver2;
-    int len;
-
-    bool operator<(const Edge& rhs) const {
-        return len < rhs.len;
-    }
-
-    bool operator>(const Edge& rhs) const {
-        return rhs < *this;
-    }
-
-    Edge(char node1, char node2, int len) : ver1(node1), ver2(node2), len(len) {}
-};
-
-const int INF = 1000000000;
-
-void bf(Edge* g[], int v, int e, int first) {
-    vector<int> d (e, INF);
-    d[first-1] =0;
-    for (int i = 0; i < v-1; ++i) {
-        for (int j = 0; j < e; ++j) {
-            d[g[j]->ver2-1] = min(d[g[j]->ver2-1], d[g[j]->ver1-1] + g[j]->len);
-            }
-        }
-    for(int i=0; i <e; ++i){
-        if(d[g[i]->ver1-1] != INF && d[g[i]->ver1-1] + g[i]->len < d[g[i]->ver2-1]){
-            cout << "Граф содержит цикл отрицательного веса\nКратчайших путей не найдено\n";
-            return;
-        }
-    }
-    for(int i=0; i<v; ++i){
-        cout << "Вершина: " << i+1 << "     " << "Путь: " << d[i] << "\n";
-    }
-}
-
-void test() {
-    Edge* graph[] = {   new Edge(1, 3, 12),
-                        new Edge(1,2,23),
-                      new Edge(2,3,25),
-                      new Edge(2,5,22),
-                      new Edge(2,8,35),
-                      new Edge(3,4,18),
-                      new Edge(4,6,20),
-                      new Edge(5,6,23),
-                      new Edge(5,7,14),
-                      new Edge(6,7,24),
-                      new Edge(7,8,16) };
-    bf(graph, 8, 11, 1);
-
-    for (auto & i : graph) {
-        delete i;
-    }
-}
-
-void input_graph() {
-    int n, l, v, n1, n2;
-    cout << "Введите кол-во вершин графа:\n";
+void Variant() {
+    cout << "Введите вариант" << endl;
+    int v;
     cin >> v;
-    cout << "Введите кол-во ребер графа:\n";
-    cin >> n;
-    cout << "Введите " << n << " ребер.\n"
-                            "В формате вершина, вес ребра, вершина.\n";
-    Edge* graph[n];
-    for (int i = 0; i < n; ++i) {
-        cin >> n1 >> l >> n2;
-        graph[i] = new Edge(n1, n2, l);
-    }
-    bf(graph, v,n,1);
-
-    for (auto & i : graph) {
-        delete i;
-    }
-}
-
-void mainMenu() {
-    cout << "-----------------------------------------------------------------------------------------" << endl;
-    cout << "Выберите команду:" << endl;
-    cout << "[1] - Вывести граф" << endl;
-    cout << "[2] - Тестовый прогон программы" << endl;
-    cout << "[0] - Завершить работу программы" << endl;
-    cout << "-----------------------------------------------------------------------------------------" << endl;
-}
-
-void menu() {
-    cout << "Выбранная команда:";
-    int choise;
-    cin >> choise;
-    cout << "----------------------------------------------------------------------------------------" << endl;
-    switch (choise) {
+    if (v > 16)
+        v = v - 16;
+    switch (v)
+    {
         case 1: {
-            input_graph();
-            menu();
+            K = round(3.2 * 10) / 10;
+            L = round(1.6 * 10) / 10;
             break;
         }
         case 2: {
-            test();
-            menu();
+            K = round(3.4 * 10) / 10;
+            L = round(1.8 * 10) / 10;
             break;
         }
-        case 0:
-            cout << "Вы точно хотите выйти?\n"
-                   "[0] Да\n"
-                   "[1] Нет\n";
-            int a;
-            cin >> a;
-            if (a == 1)
-                menu();
+        case 3: {
+            K = round(3.6 * 10) / 10;
+            L = round(2.0 * 10) / 10;
             break;
-        default:
-            cout << "Вы ввели неправильное значение." << endl;
-            menu();
+        }
+        case 4: {
+            K = round(3.8 * 10) / 10;
+            L = round(2.2 * 10) / 10;
             break;
+        }
+        case 5: {
+            K = round(4.0 * 10) / 10;
+            L = round(2.4 * 10) / 10;
+            break;
+        }
+        case 6: {
+            K = round(2.2 * 10) / 10;
+            L = round(1.2 * 10) / 10;
+            break;
+        }
+        case 7: {
+            K = round(2.4 * 10) / 10;
+            L = round(1.4 * 10) / 10;
+            break;
+        }
+        case 8: {
+            K = round(2.6 * 10) / 10;
+            L = round(1.6 * 10) / 10;
+            break;
+        }
+        case 9: {
+            K = round(2.8 * 10) / 10;
+            L = round(1.8 * 10) / 10;
+            break;
+        }
+        case 10: {
+            K = round(3.0 * 10) / 10;
+            L = round(2.2 * 10) / 10;
+            break;
+        }
+        case 11: {
+            K = round(1.2 * 10) / 10;
+            L = round(0.8 * 10) / 10;
+            break;
+        }
+        case 12: {
+            K = round(1.4 * 10) / 10;
+            L = round(1.0 * 10) / 10;
+            break;
+        }
+        case 13: {
+            K = round(1.6 * 10) / 10;
+            L = round(1.2 * 10) / 10;
+            break;
+        }
+        case 14: {
+            K = round(1.8 * 10) / 10;
+            L = round(1.4 * 10) / 10;
+            break;
+        }
+        case 15: {
+            K = round(4.2 * 10) / 10;
+            L = round(3.2 * 10) / 10;
+            break;
+        }
+        case 16: {
+            K = round(4.4 * 10) / 10;
+            L = round(3.4 * 10) / 10;
+            break;
+        }
     }
 }
 
 int main()
 {
-    mainMenu();
-    menu();
-    return 0;
+    setlocale(LC_ALL, "rus");
+    Variant();
+    float a = 0, h = 0.5;
+    vector <float> x;
+    vector <float> y;
+    x.resize(64);
+    y.resize(64);
+    a = round(((K - 1) / 2) * 10) / 10;
+    cout << "Метод Эйлера" << endl;
+    x[0] = 0;
+    y[0] = L;
+    cout << endl << "x(0) = " << x[0] << endl << "y(0) = " << y[0] << endl << "=============================" << endl << endl;
+    for (int i = 0; i < 4; i++) {
+        y[i + 1] = round((y[i] + h * (x[i] * x[i] + a * y[i])) * 1000000) / 1000000;
+        x[i + 1] = round((x[i] + 0.5) * 10) / 10;
+        cout << "x(" << i + 1 << ") = " << x[i + 1] << endl;
+        cout << "y(" << i + 1 << ") = " << y[i + 1] << endl;
+        cout << "=============================" << endl << endl;
+    }
+    x.clear();
+    y.clear();
+    x.resize(64);
+    y.resize(64);
+    cout << endl << "Метод трапеций" << endl;
+    x[0] = 0;
+    y[0] = L;
+    cout << endl << "x(0) = " << x[0] << endl << "y(0) = " << y[0] << endl << "=============================" << endl << endl;
+    for (int i = 0; i < 4; i++) {
+        x[i + 1] = round((x[i] + 0.5) * 10) / 10;
+        y[i + 1] = round((y[i] + h / 2 * ((x[i] * x[i] + a * y[i]) + (x[i + 1] * x[i + 1] + a * (y[i] + h * (x[i] * x[i] + a * y[i]))))) * 1000000) / 1000000;
+        cout << "x(" << i + 1 << ") = " << x[i + 1] << endl;
+        cout << "y(" << i + 1 << ") = " << y[i + 1] << endl;
+        cout << "=============================" << endl << endl;
+    }
+    x.clear();
+    y.clear();
+    x.resize(64);
+    y.resize(64);
+    cout << endl << "Метод Рунге-Кутты" << endl;
+    vector <float> k;
+    k.resize(64);
+    x[0] = 0;
+    y[0] = L;
+    cout << endl << "x(0) = " << x[0] << endl << "y(0) = " << y[0] << endl << "=============================" << endl << endl;
+
+    for (int i = 0; i < 4; i++) {
+        x[i + 1] = round((x[i] + 0.5) * 10) / 10;
+        k[1] = round((x[i] * x[i] + a * y[i]) * 1000000) / 1000000;
+        k[2] = round(((x[i] + h / 2) * (x[i] + h / 2) + a * (y[i] + h * k[1] / 2)) * 1000000) / 1000000;
+        k[3] = round(((x[i] + h / 2) * (x[i] + h / 2) + a * (y[i] + h * k[2] / 2)) * 1000000) / 1000000;
+        k[4] = round(((x[i] + h) * (x[i] + h) + a * (y[i] + h * k[3])) * 1000000) / 1000000;
+        y[i + 1] = round((y[i] + h / 6 * (k[1] + 2 * k[2] + 2 * k[3] + k[4])) * 1000000) / 1000000;
+        cout << "x(" << i + 1 << ") = " << x[i + 1] << endl;
+        cout << "K1[" << i << "] = " << k[1] << endl;
+        cout << "K2[" << i << "] = " << k[2] << endl;
+        cout << "K3[" << i << "] = " << k[3] << endl;
+        cout << "K4[" << i << "] = " << k[4] << endl;
+        cout << "y(" << i + 1 << ") = " << y[i + 1] << endl;
+        cout << "=============================" << endl << endl;
+    }
 }
