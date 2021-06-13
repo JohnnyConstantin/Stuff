83156f44f29acc7da97446b494c7547bc5f2c2e0
Krasav4ik <zkonst12@gmail.com>
2021-06-13 22:59:43 +0300

    Dereferrencing a pointer

--- penetrating_python/venv/main.py
+++ penetrating_python/venv/main.py
@@ -4,3 +4,7 @@ libc = CDLL("libc.dylib")
 message_string = b"Im here!"
 libc.printf(b"Fuck me: %s", message_string)
 
+seitz = c_char_p(b"loves python")# Pointer to byte array
+
+print(seitz.value) # dereferrencing a pointer so getting a contained string
+
