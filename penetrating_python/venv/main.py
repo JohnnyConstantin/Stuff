6fa4864995263c6dca99a6cf2fc48c473af83ae6
Krasav4ik <zkonst12@gmail.com>
2021-06-13 22:51:00 +0300

    Practicing with ctypes

--- penetrating_python/venv/main.py
+++ penetrating_python/venv/main.py
@@ -1,41 +1,6 @@
-"""
-Written by Johnny 2021
-"""
+from ctypes import *
 
-import threading
+libc = CDLL("libc.dylib")
+message_string = b"Im here!"
+libc.printf(b"Fuck me: %s", message_string)
 
-ip = "1.1.1"
-host = "ya.ru"
-id = "1412"
-
-
-class Factory:
-
-    def __init__(self, value1="message", value2=id, value3=host, value4=ip):
-        self.value1 = value1
-        self.value2 = value2
-        self.value3 = value3
-        self.value4 = value4
-        self.string = f"Id: {value2}, host: {value3}, ip: {value4}, msg: {value1}"
-
-    def send_to_temp(self):
-        print(f"Making actions with {self.string}")
-
-    def msg(self):
-        print(f"Sending msg")
-
-    def catch(self):
-        self.msg()
-        self.send_to_temp()
-
-
-def main():
-    print("If default:\n")
-    one = Factory("hello world")
-    one.catch()
-    two = Factory("command", "id", "host", "ip")
-    two.catch()
-
-
-if __name__ == '__main__':
-    main()
